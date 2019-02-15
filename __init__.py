5a10a831caa7731026f37354f6a71b1d047cad29
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-02-14 21:30:27 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ class Turtlerear(MycroftSkill):
     @intent_file_handler('turtlerear.intent')
     def handle_turtlerear(self, message):
         self.speak_dialog('turtlerear')
-        s = "rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-2.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'"
+        s = "rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-2.0, 0.0, 0.0]' '[2.0, 0.0, 0.0]'"
         subprocess.call([s],shell=True)
 
 def create_skill():
